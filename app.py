cf2960bb4610f8a2a5b6b2ae7e5e5b97055a26fe
Enes <enes@email.com>
2026-01-05 18:36:35 +0300

    Render port binding fix

--- app.py
+++ app.py
@@ -305,6 +305,8 @@ if __name__ == "__main__":
 
 import webbrowser
 
+import os
+
 if __name__ == "__main__":
-    init_db()
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
